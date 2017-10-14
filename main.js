a1e7f708086ea0d2e986120864ba800faf90f5b3
TPeterW <peterwangtao0@hotmail.com>
2017-10-13 21:09:27 -0400

    Log in button

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, resizable: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -16,9 +16,6 @@ function createWindow () {
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
